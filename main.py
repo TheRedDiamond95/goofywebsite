80f04b411029e051f8fef24d6dcf71fa12c0b254
TheRedDiamond <162272484+TheRedDiamond95@users.noreply.github.com>
2024-11-23 13:41:24 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app=Flask(_name_)
 def index():
-  return render_template("main.html")
+  return render_template("index.html")
 
 if _name_='_main_':
   app.run(host='0.0.0.0', port=5000)
